909f3541591b81e0b624860cd2a7d4e5109f51e7
alex baldwin <ghostbaldwin@gmail.com>
2026-07-31 14:18:46 -0400

    decap: fix logo url, point to main branch

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,7 +4,7 @@ export default function () {
       process.env.CONTEXT === "production"
         ? process.env.URL
         : process.env.CONTEXT === "dev"
-          ? `http://localhost:${process.env.PORT ?? 8888}/`
+          ? `http://localhost:${process.env.PORT ?? 8888}`
           : (process.env.DEPLOY_PRIME_URL ?? process.env.URL),
     title: "Bed-Stuy Strong",
     emailSubscribeFormUrl: process.env.HUGO_EMAIL_SUBSCRIBE_FORM_URL,
